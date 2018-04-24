0bf5ab737a865509da19579008dae67d389f6bcf
Jashka <jashka@localhost.jashka>
2018-04-24 22:39:53 +0300

    base migrations

--- database/migrations/2018_04_24_192652_create_universities_table.php
+++ database/migrations/2018_04_24_192652_create_universities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUniversitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('universities', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->string('uuid')->nullable();
-            $table->date('birthday')->nullable();
+            $table->string('position'); // json field
+            $table->string('description')->nullable();
 
             $table->timestamps();
         });
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('universities');
     }
 }

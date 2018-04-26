f2fb779cdb3b3c038d97cf7a767552a7b3fe86bc
Jashka <jashka@localhost.jashka>
2018-04-26 15:04:43 +0300

    image manager with service, secure resize, resize

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
         \App\Providers\DropboxServiceProvider::class,
         /*
          * Application Service Providers...
@@ -227,6 +227,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];

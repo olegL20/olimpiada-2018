fdbc4ce61caf8dcfbd3ecd7e2637a1c0bcbe7a50
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-05-15 20:34:37 +0300

    feature (Seeds) add users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Model\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        for ($id = 1; $id < 3; $id++) {
+            $field = User::find($id);
+
+            if (is_null($field)) {
+                $field = new User();
+                $field->name = 'name' . $id;
+                $field->surname = 'surname' . $id;
+                $field->email = 'user' . $id . '@gmail.com';
+                $field->password = bcrypt('12345678');
+                $field->role = 'user';
+                $field->confirmed = true;
+                $field->save();
+            }
+        }
+
+        for ($id = 3; $id < 6; $id++) {
+            $field = User::find($id);
+
+            if (is_null($field)) {
+                $field = new User();
+                $field->name = 'uni_admin' . $id;
+                $field->surname = 'uni_admin' . $id;
+                $field->email = 'uni_admin' . $id . '@gmail.com';
+                $field->password = bcrypt('12345678');
+                $field->role = 'uni_admin';
+                $field->confirmed = true;
+                $field->save();
+            }
+        }
+
+        $field = User::find($id = 6);
+
+        if (is_null($field)) {
+            $field = new User();
+            $field->name = 'global_admin' . $id;
+            $field->surname = 'global_admin' . $id;
+            $field->email = 'global_admin' . $id . '@gmail.com';
+            $field->password = bcrypt('12345678');
+            $field->role = 'global_admin';
+            $field->confirmed = true;
+            $field->save();
+        }
     }
 }
